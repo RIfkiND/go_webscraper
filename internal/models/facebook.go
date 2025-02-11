6a3ce7ea58f86170263e5694b184a9ee6d01f2af
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2025-02-12 06:23:50 +0700

    update

--- internal/models/facebook.go
+++ internal/models/facebook.go
@@ -1,8 +1,8 @@
 package models
 
-// FacebookPost represents the metadata for a Facebook post
+
 type FacebookPost struct {
-	Title       string `json:"title"`        // Title of the post
-	Description string `json:"description"`  // Post description
-	ImageURL    string `json:"image_url"`    // URL of the featured image
+	Title       string `json:"title"`        
+	Description string `json:"description"` 
+	ImageURL    string `json:"image_url"`    
 }
--- internal/models/tiktok.go
+++ internal/models/tiktok.go
@@ -1,9 +1,9 @@
 package models
 
 type TikTokVideo struct{
-	Username     string `json:"username"`      // TikTok username
-	Caption      string `json:"caption"`       // Video caption
-	VideoURL     string `json:"video_url"`     // Direct video URL
-	LikeCount    int    `json:"like_count"`    // Number of likes
-	CommentCount int    `json:"comment_count"` // Number of comments
+	Username     string `json:"username"`      
+	Caption      string `json:"caption"`      
+	VideoURL     string `json:"video_url"`     
+	LikeCount    int    `json:"like_count"`   
+	CommentCount int    `json:"comment_count"` 
 }
